c2e0ef595b00802e0bbf7e3fec93a1e729bce5ce
Dastan <ddbolatkhan@gmail.com>
2025-06-03 22:23:17 +0500

    дописывал корзину но она не работает

--- main/views.py
+++ main/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.core.paginator import Paginator
+from cart.forms import CartAddProductForm
 
 def popular_list(request):
     products = Product.objects.filter(available=True)[:3]
     return render(request, 'main/index/index.html', {'products': products})
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, available=True)
-    return render(request, 'main/product/detail.html', {'product': product})
+    product = get_object_or_404(Product, 
+                                slug=slug, 
+                                available=True)
+    cart_product_form = CartAddProductForm
+    return render(request, 
+                'main/product/detail.html', 
+                {'product': product, 
+                'cart_product_form': cart_product_form})
 
 def product_list(request, category_slug=None):
     page = request.GET.get('page', 1)
