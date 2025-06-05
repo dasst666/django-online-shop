a64e808ff0deb66bee077b754ec2dd551c22658c
Dastan <ddbolatkhan@gmail.com>
2025-06-05 21:10:21 +0500

    поставил postgres и добавил шрифт в стили

--- heavyaura/settings.py
+++ heavyaura/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'heavyaura.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'heavyaura',
+        'USER': 'heavyaura',
+        'PASSWORD': 'heavyaura',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
